a9cf8f63ce14afca77eddc76c15c943c3e012f76
pitakt80 <pitakt80@hotmail.com>
2018-10-13 09:35:17 +0700

    Fix build fail when run npm start build command

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const app = electron.app;
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
 
-require('electron-reload')(__dirname+'/public');
+//require('electron-reload')(__dirname+'/public');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
